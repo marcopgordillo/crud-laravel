1347a1e6a8a1c1661ec84ac3d516ab1e64a7d620
marcopgordillo <6589245+marcopgordillo@users.noreply.github.com>
2019-11-28 10:19:48 -0500

    Add a crud app

---
+++ database/factories/CrudFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Crud;
+use Faker\Generator as Faker;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| Here you may define all of your model factories. Model factories give
+| you a convenient way to create models for testing and seeding your
+| database. Just tell the factory how a default model should look.
+|
+*/
+$factory->define(Crud::class, function (Faker $faker) {
+    return [
+        'name' => $faker->lexify('????????'),
+        'color' =>$faker->boolean ? 'red' : 'green'
+    ];
+});
